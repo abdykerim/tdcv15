fb197218e6886b48d87b2555228262c6f4eec221
abdykerim <abdykerim.erikov@gmail.com>
2015-11-03 16:24:29 +0100

    - combined gaussian filter in one file, no need for convolution function anymore - first part of exercise is in presentalbe form, hopefully no more changes needed

--- exercise2/exercise2_1.m
+++ exercise2/exercise2_1.m
@@ -1,15 +1,48 @@
 lena = imread('lena.gif');
 lena = im2double(lena);
-%figure('Name', 'original');
-%imshow(lena);
-%SPN2 = imnoise(lena,'salt & pepper',0.05);  <- FUNCTION PROVIDED BY MATLAB
-SPN = salt_pepper_noise(lena, 0.05);
-C = median_filter(SPN, 3);
-figure('Name', '1.a salt-pepper noise and median filter');
-imshowpair(SPN,C,'montage')
+%figure('Name', '1.a salt-pepper noise and median filter');
+%imshowpair(SPN,MM,'montage')
+figure('Name', 'Exercise 1');
+subplot(3,3,1);
+imshow(lena);
+title('original');
 
-GN = gaussian_noise(lena, 0.02, 0);
-Gaussian2D = gaussian_kernel([9,9], 3);
-G1 = convolution(GN, Gaussian2D , 'm');
-figure('Name', '1.a gaussian noise and gaussian filter');
-imshowpair(GN,G1,'montage')
+GO = gaussian_filter(lena, [9, 9], 3);
+subplot(3,3,4);
+imshow(GO);
+title('gaussian filter on original');
+
+MO = median_filter(lena, 3);
+subplot(3,3,7);
+imshow(MO);
+title('median filter on original');
+
+SPN = salt_pepper_noise(lena, 0.02);
+subplot(3,3,2);
+imshow(SPN);
+title('salt and pepper noise');
+
+GS = gaussian_filter(SPN, [9, 9], 3);
+subplot(3,3,5);
+imshow(GS);
+title('gaussian filter on salt and pepper');
+
+MS = median_filter(SPN, 3);
+subplot(3,3,8);
+imshow(MS);
+title('median filter on salt and pepper');
+
+GN = gaussian_noise(lena, 0.01, 0);
+subplot(3,3,3);
+imshow(GN);
+title('gassian noise');
+
+GG = gaussian_filter(GN, [9, 9], 3);
+subplot(3,3,6);
+imshow(GG);
+title('gaussian filter on Gaussian');
+
+MG = median_filter(GN, 3);
+subplot(3,3,9);
+imshow(MG);
+title('median filter on Gaussian');
---
+++ exercise2/gaussian_filter.m
@@ -0,0 +1,22 @@
+function out = gaussian_filter(im, dim, sigma)
+
+    ratio_x = floor(dim(1)/2) - (1 - mod(dim(1),2));   
+    ratio_y = floor(dim(2)/2) - (1 - mod(dim(2),2));
+    
+    [X,Y] = meshgrid(-ratio_x : ratio_x , -ratio_y : ratio_y);
+    
+    % build gaussian filter
+    G = exp(-(X.^2+Y.^2)/(2*sigma^2));
+    
+    % normalize
+    G = G ./ sum(sum(G));
+    
+    %// Convert filter into a column vector
+    G = G(:);
+
+    %// Filter our image
+    I_pad = padarray(im, [floor(dim(1)/2) floor(dim(2)/2)], 'symmetric', 'both');
+    C = im2col(I_pad, [dim(1) dim(2)], 'sliding');
+    C_filter = sum(bsxfun(@times, C, G), 1);
+    out = col2im(C_filter, [dim(1) dim(2)], size(I_pad), 'sliding');  
+end
