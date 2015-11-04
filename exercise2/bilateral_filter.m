defc1bdc69a5d5face7c829da1f6b35da6b3d565
abdykerim <abdykerim.erikov@gmail.com>
2015-11-04 13:11:22 +0100

    - added exercise 2, bilateral filter needs some debugging to put correct padding

--- exercise2/bilateral_filter.m
+++ exercise2/bilateral_filter.m
@@ -1,4 +1,20 @@
-function = bilateral_filter( dim )
+function y = bilateral_filter( im, sigma_d, sigma_r)
+       
+    [M,N] = size(im);
+    y = zeros(size(im));
 
+    r = floor(sigma_d*3/2);     % Adjust for desired window size
+   % im_pad = padarray(im, [r r], 'symmetric', 'both');
+    for n = 1+r:N-r
+        for m = 1+r:M-r
+            % Extract a window of size (2r+1)x(2r+1) around (m,n)
+            w = im(m+(-r:r),n+(-r:r));
+            % The bilateral filter
+            [k,j] = meshgrid(-r:r,-r:r);
+            h = exp( -(j.^2 + k.^2)/(2*sigma_r^2) ) .* ...
+                exp( -(w - w(r+1,r+1)).^2/(2*sigma_d^2) );
+            y(m,n) = h(:)'*w(:) / sum(h(:));
+        end
+    end
 end
 
