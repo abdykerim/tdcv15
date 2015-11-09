31add4ec8631347c38486e68f8ee828c427d207a
Marcel <marcel.cordovi.s@gmail.com>
2015-11-09 10:44:02 +0100

    small fix

--- exercise sheet 3/exercise_1a.m
+++ exercise sheet 3/exercise_1a.m
@@ -2,8 +2,8 @@ close all
 
 % [tunnel, map, alpha] = imread('checkerboard_tunnel.png', 'png');
 % I = im2double(tunnel);
-[tunnel, map, alpha] = imread('sample2.jpg', 'jpg');
-I = rgb2gray(im2double(tunnel));
+[testImg, map, alpha] = imread('sample2.jpg', 'jpg');
+I = rgb2gray(im2double(testImg));
 
 % params
 
@@ -12,15 +12,7 @@ sigma_i = 1;
 G_si_size = 9;
 threshold = 0.005;
 
-% R = harris_corner_detector(I, sigma_i, sigma_d, G_si_size, threshold);
-% R(R<threshold) = 0;
-% R = imregionalmax(R);
-
-
 harris_laplace(I, 4, 2, G_si_size, threshold);
-
-
-
 
 % [r,c] = find(vec2mat(R, size(I, 2)));  % Find row,col coords.	
 % figure, imagesc(I), axis image, colormap(gray), hold on
--- exercise sheet 3/harris_laplace.m
+++ exercise sheet 3/harris_laplace.m
@@ -10,25 +10,20 @@ function R = harris_laplace( inputImage, scale_level, sigma_0, gSize, t )
         
     harris_f = @ (s_i) multiscale_harris(inputImage, s_i, gSize, t);    
     L_harris = arrayfun(harris_f, s_i_params, 'un',0);
-    
-    
-    
-    figure;
-    imshow(deepH);
-    
+        
     laplace_f = @ (s) laplacian(inputImage, s);
     L_laplacian = arrayfun(laplace_f, s_i_params, 'un',0);
                 
-    figure;
+    figure('Name', 'Laplacians');
     for i = 1:size(L_laplacian,2)
         subplot(2,size(L_laplacian,2) / 2,i);
         imshow(cell2mat(L_laplacian(i)));
     end    
     
-    figure;
+    figure('Name', 'Harris');
     for i = 1:size(L_harris,2)
         subplot(2,size(L_harris,2) / 2,i);
-        imshow(100*cell2mat(L_harris(i)));
+        imshow(100*cell2mat(L_harris(i)));        
     end  
     
 end
