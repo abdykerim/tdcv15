aa73c19f5802aaedfc2b4ca4a9cb00401d22fe55
abdykerim <abdykerim.erikov@gmail.com>
2015-11-26 11:04:22 +0100

    ex5 submission
    final submission

--- exercise5/integral_images.m
+++ exercise5/integral_images.m
@@ -1,9 +1,4 @@
 function [integralI] = integral_images(I)
-% without padding zeros on top and left
-% R = cumsum(cumsum(double(I(:,:,1))')');
-% G = cumsum(cumsum(double(I(:,:,2))')');
-% B = cumsum(cumsum(double(I(:,:,3))')');
-
 % with padded zeros on top and left
 integralI = zeros(size(I,1)+1, size(I,2)+1, size(I,3));
 integralI (:,:, 1) = integralImage(double(I(:,:,1)));
