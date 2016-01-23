6ac712d0ad22013c149590e402bc566a72f4b99d
Marcel <marcel.cordovi.s@gmail.com>
2016-01-23 11:30:50 +0100

    subImage.m

--- exercice_10/subImage.m
+++ exercice_10/subImage.m
@@ -1,9 +1,10 @@
 function [i, x, y] = subImage( hsv_img, center, w, h )
     
-    h = floor(h/2) + mod(h,2);
-    w = floor(w/2) + mod(w,2);
+    h = floor(h/2) + mod(round(h),2);
+    w = floor(w/2) + mod(round(w),2);
     x = center(1)-h;
     y = center(2)-w;
+        
     i = hsv_img(center(1)-h:center(1)+h, center(2)-w:center(2)+w, :);
 
 end
